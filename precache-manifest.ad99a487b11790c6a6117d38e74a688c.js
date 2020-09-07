1320b206e50f0a7045029ac5f95ffb7ee22429de
Moro Moses <moro_moses@hotmail.com>
2020-09-07 12:50:56 +0300

    Updates

---
+++ precache-manifest.ad99a487b11790c6a6117d38e74a688c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba170b68e6d24cf26fc0152e8e5964d0",
+    "url": "/gane/index.html"
+  },
+  {
+    "revision": "b400a83964e89e2193f3",
+    "url": "/gane/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "58d2f446ac3d1aa7baff",
+    "url": "/gane/static/css/main.9e6dc747.chunk.css"
+  },
+  {
+    "revision": "b400a83964e89e2193f3",
+    "url": "/gane/static/js/2.4f2417f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gane/static/js/2.4f2417f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58d2f446ac3d1aa7baff",
+    "url": "/gane/static/js/main.72df7ff4.chunk.js"
+  },
+  {
+    "revision": "8388854b6b3596e54f5a",
+    "url": "/gane/static/js/runtime-main.125a589c.js"
+  }
+]);
